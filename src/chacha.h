56eda185b4c5b73a35e0da4ba8f9a806a52c7f5b
Travis Mick <lq@le1.ca>
2015-05-16 16:55:52 -0600

    prototype chacha_encrypt function

--- src/chacha.h
+++ src/chacha.h
@@ -2,9 +2,11 @@
 #define _chacha_h_
 
 #include <stdint.h>
+#include <string.h>
 
 #define ROTL_32(x, n) x = (x << n) | (x >> (32 - n))
 
+/* sizes in 32-bit words */
 #define CHACHA_STATE_SIZE (16)
 #define CHACHA_KEY_SIZE    (8)
 #define CHACHA_NONCE_SIZE  (3)
@@ -19,4 +21,6 @@ typedef uint32_t chacha_counter;
 void chacha_quarter_round(chacha_state s, const chacha_round_def d);
 void chacha_state_setup(chacha_state s, chacha_key k, chacha_nonce iv, chacha_counter c);
 void chacha_block(chacha_state s);
+void chacha_encrypt(uint32_t counter, const unsigned char key[32], const unsigned char iv[12], const unsigned char *input, unsigned char *output, size_t len);
+
 #endif
