d3911a105993ae275300a2721eb7d102425aa8ac
Travis Mick <lq@le1.ca>
2015-05-16 00:19:47 -0600

    implement block function

--- src/chacha.c
+++ src/chacha.c
@@ -1,5 +1,7 @@
 #include "chacha.h"
 
+#include <string.h>
+
 static const chacha_round_def CHACHA_ROUND_DEFS[8] = {
         {0, 4,  8, 12},
         {1, 5,  9, 13},
@@ -11,7 +13,11 @@ static const chacha_round_def CHACHA_ROUND_DEFS[8] = {
         {3, 4,  9, 14}
 };
 
-void chacha_quarter_round(chacha_state s, chacha_round_def d){
+static const uint32_t CHACHA_MAGIC_NUMBERS[4] = {
+        0x61707865, 0x3320646e, 0x79622d32, 0x6b206574
+};
+
+void chacha_quarter_round(chacha_state s, const chacha_round_def d){
         s[d[0]] += s[d[1]];
         s[d[3]] ^= s[d[0]];
         ROTL_32(s[d[3]], 16);
@@ -27,4 +33,41 @@ void chacha_quarter_round(chacha_state s, chacha_round_def d){
         s[d[2]] += s[d[3]];
         s[d[1]] ^= s[d[2]];
         ROTL_32(s[d[1]], 7);
+}
+
+void chacha_state_setup(chacha_state s, chacha_key k, chacha_nonce iv, chacha_counter c){      
+        s[0]  = CHACHA_MAGIC_NUMBERS[0];
+        s[1]  = CHACHA_MAGIC_NUMBERS[1];
+        s[2]  = CHACHA_MAGIC_NUMBERS[2];
+        s[3]  = CHACHA_MAGIC_NUMBERS[3];
+        s[4]  = k[0];
+        s[5]  = k[1];
+        s[6]  = k[2];
+        s[7]  = k[3];
+        s[8]  = k[4];
+        s[9]  = k[5];
+        s[10] = k[6];
+        s[11] = k[7];
+        s[12] = c;
+        s[13] = iv[0];
+        s[14] = iv[1];
+        s[15] = iv[2];
+}
+
+void chacha_block(chacha_state s){
+        unsigned char i, j;
+       
+        chacha_state t;
+        memcpy(t, s, CHACHA_STATE_SIZE * sizeof(uint32_t));
+        
+        for(i = 0; i < 10; i++){
+                for(j = 0; j < 8; j++){
+                        chacha_quarter_round(t, CHACHA_ROUND_DEFS[j]);
+                }
+        }
+        
+        for(i = 0; i < CHACHA_STATE_SIZE; i++){
+                s[i] += t[i];
+        }
+
 }
--- src/chacha.h
+++ src/chacha.h
@@ -5,9 +5,18 @@
 
 #define ROTL_32(x, n) x = (x << n) | (x >> (32 - n))
 
-typedef uint32_t chacha_state[16]; 
+#define CHACHA_STATE_SIZE (16)
+#define CHACHA_KEY_SIZE    (8)
+#define CHACHA_NONCE_SIZE  (3)
+
+typedef uint32_t chacha_state[CHACHA_STATE_SIZE]; 
 typedef uint8_t chacha_round_def[4];
 
-void chacha_quarter_round(chacha_state s, chacha_round_def d);
+typedef uint32_t chacha_key[CHACHA_KEY_SIZE];
+typedef uint32_t chacha_nonce[CHACHA_NONCE_SIZE];
+typedef uint32_t chacha_counter;
 
+void chacha_quarter_round(chacha_state s, const chacha_round_def d);
+void chacha_state_setup(chacha_state s, chacha_key k, chacha_nonce iv, chacha_counter c);
+void chacha_block(chacha_state s);
 #endif
